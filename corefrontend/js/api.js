11b4c8a8c328b69b8b4cbd59a3e5db7843d7d5db
10xdrink <ittenex@gmail.com>
2026-03-12 01:39:34 +0530

    fix: use relative /api path on production to avoid mixed content HTTPS error

--- corefrontend/js/api.js
+++ corefrontend/js/api.js
@@ -1,6 +1,6 @@
 const API_BASE = (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1')
   ? 'http://localhost:8000/api'
-  : 'http://' + window.location.hostname + ':8000/api';
+  : '/api';
 
 
 async function apiFetch(path, options = {}) {
